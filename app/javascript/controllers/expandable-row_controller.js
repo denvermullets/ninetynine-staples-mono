e242179c82d94255f4ec5a3fd596f4c7ffa1ef32
denvermullets <devmullets@gmail.com>
2024-10-04 15:19:06 -0400

    fix(boxsets): add row toggle

--- app/javascript/controllers/expandable-row_controller.js
+++ app/javascript/controllers/expandable-row_controller.js
@@ -4,20 +4,18 @@ export default class extends Controller {
   static targets = ["content"];
 
   toggle(event) {
-    // Get the card ID from the clicked element
-    console.log("hi");
     const cardId = event.currentTarget.dataset.cardId;
-    console.log("cardId: ", cardId);
 
-    // Find the corresponding content target that matches the card ID
     const contentRow = this.contentTargets.find((content) => {
       return content.dataset.cardId === cardId;
     });
-    console.log("contentRow: ", contentRow);
 
-    // Toggle the visibility of the matching content row
     if (contentRow) {
       contentRow.classList.toggle("hidden");
+
+      if (!contentRow.classList.contains("hidden")) {
+        contentRow.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
     }
   }
 }
