324ec5cd109b369987d2460d6bed4cf10c369407
denvermullets <devmullets@gmail.com>
2025-01-16 09:58:23 -0500

    fix(rubocop): address lint errors

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,8 +1,7 @@
 class CollectionsController < ApplicationController
-
   def show
-    user = User.find_by(username: params[:username])
-    collection = Collection.where('LOWER(name) = ? AND user_id = ?', params[:binder], user.id)
+    # user = User.find_by(username: params[:username])
+    # collection = Collection.where('LOWER(name) = ? AND user_id = ?', params[:binder], user.id)
 
     @options = load_boxset_names
 
