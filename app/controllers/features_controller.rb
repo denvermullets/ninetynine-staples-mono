fd1120af6f9f27a196fa102ec0ea641b3e87f015
denvermullets <devmullets@gmail.com>
2026-01-28 06:44:14 -0500

    feat(features): add card bg

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,3 +1,12 @@
 class FeaturesController < ApplicationController
-  def show; end
+  def show
+    min_id = MagicCard.minimum(:id)
+    max_id = MagicCard.maximum(:id)
+    ids = Array.new(20) { rand(min_id..max_id) }
+
+    @hero_cards = MagicCard
+                  .where.not(image_medium: [nil, ''])
+                  .where(id: ids, is_token: false)
+                  .limit(9)
+  end
 end
